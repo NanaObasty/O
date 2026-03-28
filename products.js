f17c77a6e4b840069496f037da6955b97c341946
NanaObasty <hrmyawobasty@icloud.com>
2026-03-28 11:43:57 +0000

    Update products.js

--- products.js
+++ products.js
@@ -1,12 +1,15 @@
 // products.js - The "Brain" of QuickTrade.gh
 const ghanaProducts = [
     { 
+     { 
         id: 1, 
         name: "Kente Print Summer Dress", 
         category: "Dresses", 
         price: 250, 
         location: "Koforidua", 
-        image: "https://www.pinterest.com/pin/9007267999689081/", 
+        inStock: true,
+        // FIXED: Direct Image Link
+        image: "https://i.pinimg.com/736x/01/91/92/019192807f66e0019482598379689081.jpg", 
         description: "Beautiful authentic Kente print. Perfect for church or weddings. Size: Medium." 
     },
     { 
@@ -15,9 +18,11 @@ const ghanaProducts = [
         category: "Sandals", 
         price: 120, 
         location: "Kumasi", 
-        image: "https://www.pinterest.com/pin/119556565104270678/", 
-        description: "Durable Northern Region leather. Very comfortable for daily wear." 
-    },
+        inStock: true,
+        // FIXED: Direct Image Link
+        image: "https://i.pinimg.com/736x/1a/6b/55/1a6b556565104270678d214f4e1f5e5e.jpg", 
+        description: "Durable Northern Region leather. Very comfortable for daily use."
+    } // <--- I added this bracket and comma back in!
     { 
         id: 3, 
         name: "Official Men's Shoes", 
